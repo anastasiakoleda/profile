259659767553d2113843017b038acb29017f427d
Anstasia <nastasiakoleda@gmil.com>
2018-03-28 14:19:35 +0300

    Currency Converter added

--- currency calculator/script.js
+++ currency calculator/script.js
@@ -2,8 +2,10 @@ var currencies = ["EUR", "GBP", "USD"]
 var rates = {}
 
 function getRates(callback) {
+    // !!!! GET YOUR OWN KEY FROM https://fixer.io/quickstart
+    var key = "46bfcd7247f69c6ba472c6c97b72c34b"
     $.ajax({
-        url: 'http://data.fixer.io/api/latest?access_key=62dc1ae246f706fc7cc02843a3b1ee63',
+        url: 'http://data.fixer.io/api/latest?access_key=' + key,
         dataType: 'jsonp',
         success: function(json) {
             rates.GBP = json.rates.GBP
@@ -19,18 +21,20 @@ function showApp() {
 }
 
 function changeFrom () {
+    // Reset to currencies
     $("#to").html('<option value="">To</option>')
     $("#result").hide()
     $("#converted").text("")
 
+    // Get selected from currency
     var selected = $("#from").val()
 
     if (selected) {
-        var newArray = currencies.slice() 
-        var index = newArray.indexOf(selected) 
-        newArray.splice(index, 1) 
+        var newArray = currencies.slice() // Copy array
+        var index = newArray.indexOf(selected) // Find from currency
+        newArray.splice(index, 1) // Remove it so it wont show on to
 
-       
+        // Print to currencies
         for (var i = 0; i < newArray.length; i++) {
             var _currency =newArray[i]
             var html = '<option value="'+_currency+'">'+_currency+'</option>'
